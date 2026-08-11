b176529fc9f6ec583cb3ea4cf165422f84ed2347
ShivaTeja-2510 <shivatejaarva25@gmail.com>
2026-08-11 15:46:36 +0530

    first commit

---
+++ Python practice/factors.py
@@ -0,0 +1,14 @@
+n = int(input("Enter number: "))
+
+factors = []
+
+for i in range(1, int(n ** 0.5) + 1):
+    if n % i == 0:
+        factors.append(i)
+
+        if i != n // i:
+            factors.append(n // i)
+
+factors.sort()
+
+print("Factors:", *factors)
